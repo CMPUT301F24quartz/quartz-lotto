2a4841aa79c9283a7a5e9a372c7d14150a04df4d
Francis Garcia <fgarcia@ualberta.ca>
2024-11-06 22:06:10 -0700

    firebase notif service.

--- code/app/build.gradle.kts
+++ code/app/build.gradle.kts
@@ -32,7 +32,9 @@ android {
     }
 }
 
-dependencies { implementation(libs.appcompat)
+dependencies {
+    implementation(libs.firebase.messaging)
+    implementation(libs.appcompat)
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
